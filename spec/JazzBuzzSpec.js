9c34ac2d747bdc916528bbb4c356630244efa2f3
Ben <ben.lynch1931@gmail.com>
2020-11-02 13:53:42 +0000

    Successfully created a JazzBuzz game (AKA FizzBuzz) using a guided walkthough as help

--- spec/JazzBuzzSpec.js
+++ spec/JazzBuzzSpec.js
@@ -13,6 +13,71 @@ describe('JazzBuzz', function() {
       expect(jazzbuzz.isDivisibleByThree(3)).toBe(true);
 
     });
+  });
+
+  describe('knows when a number isn\'t', function() {
+
+    it('is divisible by 3', function() {
+
+      expect(jazzbuzz.isDivisibleByThree(4)).toBe(false);
+
+    });
+
+  });
+
+  describe('knows when a number is', function() {
+
+    it('is divisible by 5', function() {
+
+      expect(jazzbuzz.isDivisibleByFive(5)).toBe(true);
+
+    });
+  });
+
+  describe('knows when a number isn\'t', function() {
+
+    it('is divisible by 5', function() {
+
+      expect(jazzbuzz.isDivisibleByFive(4)).toBe(false);
+
+    });
+  });
+
+  describe('knows when a number is', function() {
+
+    it('is divisible by 15', function() {
+
+      expect(jazzbuzz.isDivisibleByFifteen(30)).toBe(true);
+
+    });
+  });
+
+  describe('knows when a number isn\'t', function() {
+
+    it('is divisible by 15', function() {
+
+      expect(jazzbuzz.isDivisibleByFifteen(31)).toBe(false);
+
+    });
+  });
+
+  describe('when playing, says', function() {
+
+    it('"Jazz" when a number is divisible by 3', function() {
+      expect(jazzbuzz.says(18)).toEqual("Jazz");
+    });
+
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(jazzbuzz.says(70)).toEqual("Buzz");
+    });
+
+    it('"JazzBuzz" when a number is divisible by 15', function() {
+      expect(jazzbuzz.says(75)).toEqual("JazzBuzz");
+    });
+
+    it('4 as this number is not divisible by 3, 5 or both', function() {
+      expect(jazzbuzz.says(4)).toEqual(4);
+    });
 
   });
 
--- src/JazzBuzz.js
+++ src/JazzBuzz.js
@@ -1,5 +1,34 @@
 class JazzBuzz {
-  isDivisibleByThree(number) {
-    return (number % 3 === 0);
+
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return "JazzBuzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+      return "Jazz";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz"
+    }
+    else {
+      return number;
+    }
   }
+
+  isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+  }
+
 }
